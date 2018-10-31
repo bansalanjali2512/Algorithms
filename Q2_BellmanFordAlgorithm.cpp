e9c754ff3f4b250f8fe4ad4e06bbadc738d65ce2
Anjali Bansal <anjali.mca17.du@gmail.com>
2018-10-31 23:49:57 +0530

    Q2_BellmanFordAlgorithm.cpp

---
+++ Q2_BellmanFordAlgorithm.cpp
@@ -0,0 +1,39 @@
+/*
+    Q.2: From a given vertex in a weighted connected graph, find shortest
+    paths to other vertices using Bellman’s ford algorithm.
+    Submitted By: Anjali Bansal (04)
+    Date: 24th Oct, 2018
+*/
+
+#include <iostream>
+using namespace std;
+
+//******************************
+//function declaration
+//******************************
+
+
+
+//******************************
+//main function
+//******************************
+
+int main () {
+    /*
+        Objective       : From a given vertex in a weighted connected graph, find shortest
+                          paths to other vertices using Bellman’s ford algorithm.
+        Input parameters: None
+        Return Value    : Integer
+    */
+
+
+    cout << endl;
+    return 0;
+}
+
+
+//******************************
+//function definition
+//******************************
+
+
